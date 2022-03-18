46d975e09654d3e8fabd6ab97c96123d58af62bf
guanyang <guanyangsunlight@163.com>
2022-03-18 16:16:06 +0800

    update femas demo

---
+++ femas-demo/femas-consumer/src/test/java/org/gy/demo/femas/consumer/FemasConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gy.demo.femas.consumer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemasConsumerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ femas-demo/femas-provider/src/test/java/org/gy/demo/femas/provider/FemasProviderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gy.demo.femas.provider;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemasProviderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
